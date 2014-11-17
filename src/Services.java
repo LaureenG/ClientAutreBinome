38a2de8f9bb7e4148352485dfe18a0cb2390e8dc
couste <couste@polytech.unice.fr>
2014-11-17 15:20:54 +0100

    Ajout de tout _ TODO : implementer la classe service

---
+++ src/Services.java
@@ -0,0 +1,83 @@
+
+public class Services {
+
+	public static String deleteNom(String string) { // "supprimer_nom"
+		// TODO : todo
+		return "";
+	}
+
+	public static String deleteSurnom(String string) { // "supprimer_surnom
+		// TODO : todo
+		return "";
+	}
+
+	public static String getSurnom(String string) { // "lister" & p=NOM
+		// TODO : todo
+		return "";
+	}
+
+	public static String getSurnom() { // "lister" & p="surnom"
+		// TODO : todo
+		return "";
+	}
+
+	public static String getName() { // "lister" & p="nom"
+		// TODO : todo
+		return "";
+	}
+
+	public static String getAll() { // "lister" & p="tout"
+		// TODO : todo
+		return "";
+	}
+
+	public static String postSurname(String string, String string2) { // "ajouter_nom" & NAME & NAME2
+		// TODO : todo
+		return "";
+	}
+
+	public static String postName(String string, String string2) { // "modifier_nom" & NAME & NAME 2
+		// TODO : todo
+		return "";
+	}
+
+	public static String putSurname(String string, String string2) { // "modifier_surnom" & NAME & NAME2
+		// TODO : todo
+		return "";
+	}
+
+	/*private static String fromReplyToString(Query query, Reply reply) {
+		// TODO : todo
+		return "";
+	}*/
+
+	private static String serviceName(int service) {
+		/*switch (service) {
+		case Service.DELETE_NAME:
+			return "DELETE_NAME";
+		case Service.DELETE_SURNAME:
+			return "DELETE_SURNAME";
+		case Service.GET_ALL:
+			return "GET_ALL";
+		case Service.GET_NAME:
+			return "GET_NAME";
+		case Service.GET_SURNAME:
+			return "GET_SURNAME";
+		case Service.POST_NAME:
+			return "POST_NAME";
+		case Service.POST_SURNAME:
+			return "POST_SURNAME";
+		case Service.PUT_SURNAME:
+			return "PUT_SURNAME";
+		default:
+			return "UNKNOWN_SERVICE";
+		}*/
+		// TODO : todo
+		return "";
+	}
+
+	public static String close() { // "exit"
+		// TODO : todo
+		return "";
+	}
+}
