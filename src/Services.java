b068bb7cff8c66e6dcfd1cb3d90be083c61a9a99
Laureen Ginier <ginier.laureen@etu.unice.fr>
2014-11-17 16:36:44 +0100

    Ajout des services

--- src/Services.java
+++ src/Services.java
@@ -1,80 +1,121 @@
 import connexion.Connexion;
 
-
 public class Services {
-	private static int id = 0;
-	public static String deleteNom(String string) { // "supprimer_nom"
-		// TODO : todo
-		return "";
-	}
+    private static int id = 0;
 
-	public static String deleteSurnom(String string) { // "supprimer_surnom
-		// TODO : todo
-		return "";
-	}
+    public static String deleteNom(String string) { // "supprimer_nom"
+        String args[] = new String[3];
+        args[0] = "\"requete\" : \"supprimer_nom\"";
+        args[1] = "\"parametre\" : \"" + string + "\"";
+        args[2] = "\"id\" : " + ++id + "";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	public static String getSurnom(String string) { // "lister" & p=NOM
-		// TODO : todo
-		return "";
-	}
+    public static String deleteSurnom(String string) { // "supprimer_surnom
+        String args[] = new String[3];
+        args[0] = "\"requete\" : \"supprimer_surnom\"";
+        args[1] = "\"parametre\" : \"" + string + "\"";
+        args[2] = "\"id\" : " + ++id + "";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	public static String getSurnom() { // "lister" & p="surnom"
-		// TODO : todo
-		return "";
-	}
+    public static String getSurnom(String string) { // "lister" & p=NOM
+        String args[] = new String[3];
+        args[0] = "\"requete\" : \"lister\"";
+        args[1] = "\"parametre\" : \"" + string + "\"";
+        args[2] = "\"id\" : " + ++id + "";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	public static String getName() { // "lister" & p="nom"
-		// TODO : todo
-		return "";
-	}
+    public static String getSurnom() { // "lister" & p="surnom"
+        String args[] = new String[3];
+        args[0] = "\"requete\" : \"lister\"";
+        args[1] = "\"parametre\" : \"surnom\"";
+        args[2] = "\"id\" : " + ++id + "";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	public static String getAll() { // "lister" & p="tout"
-		String [] args = new String[4];
-		args[0] = "\"requete\" : \"lister\"";
-		args[1] = "\"parametre\" : \"tout\"";
-		args[2] = "\"id\" : "+ ++id +"";
-		args[3] = null;
-		String query = createQuery(args);
-		Connexion.getInstance().sendString(query);
-		return Connexion.getInstance().receiveString();
-	}
+    public static String getName() { // "lister" & p="nom"
+        String args[] = new String[3];
+        args[0] = "\"requete\" : \"lister\"";
+        args[1] = "\"parametre\" : \"nom\"";
+        args[2] = "\"id\" : " + ++id + "";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	private static String createQuery(String[] args) {
-		String toRet = "{";
-		for(int i = 0 ; i < args.length ; i++) {
-			if(args[i]!=null) {
-				toRet += args[i];
-			}
-			if(i != args.length-1 && args[i+1] != null) {
-				toRet += ",";
-			}
-		}
-		toRet += "}\n";
-		return toRet;
-	}
+    public static String getAll() { // "lister" & p="tout"
+        String[] args = new String[3];
+        args[0] = "\"requete\" : \"lister\"";
+        args[1] = "\"parametre\" : \"tout\"";
+        args[2] = "\"id\" : " + ++id + "";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	public static String postSurname(String string, String string2) { // "ajouter_nom" & NAME & NAME2
-		// TODO : todo
-		return "";
-	}
+    private static String createQuery(String[] args) {
+        String toRet = "{";
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] != null) {
+                toRet += args[i];
+            }
+            if (i != args.length - 1 && args[i + 1] != null) {
+                toRet += ",";
+            }
+        }
+        toRet += "}\n";
+        return toRet;
+    }
 
-	public static String postName(String string, String string2) { // "modifier_nom" & NAME & NAME 2
-		// TODO : todo
-		return "";
-	}
+    public static String postSurname(String string, String string2) { // "modifier_nom" & NAME & NAME2
+        String args[] = new String[4];
+        args[0] = "\"requete\" : \"modifier_surnom\"";
+        args[1] = "\"parametre_surnom\" : \""+string+"\"";
+        args[2] = "\"parametre_nouveau_surnom\" : \""+string2+"\"";
+        args[3] = "\"id\" : "+ ++id +"";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	public static String putSurname(String string, String string2) { // "modifier_surnom" & NAME & NAME2
-		// TODO : todo
-		return "";
-	}
+    public static String postName(String string, String string2) { // "modifier_nom" & NAME & NAME 2
+        String args[] = new String[4];
+        args[0] = "\"requete\" : \"modifier_nom\"";
+        args[1] = "\"parametre_nom\" : \""+string+"\"";
+        args[2] = "\"parametre_surnom\" : \""+string2+"\"";
+        args[3] = "\"id\" : "+ ++id +"";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	/*private static String fromReplyToString(Query query, Reply reply) {
-		// TODO : todo
-		return "";
-	}*/
+    public static String putSurname(String string, String string2) { // "ajouter_surnom"& NAME &NAME2
+        String args[] = new String[4];
+        args[0] = "\"requete\" : \"ajouter_nom\"";
+        args[1] = "\"parametre_nom\" : \""+string+"\"";
+        args[2] = "\"parametre_nouveau_nom\" : \""+string2+"\"";
+        args[3] = "\"id\" : "+ ++id +"";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 
-	public static String close() { // "exit"
-		// TODO : todo
-		return "";
-	}
+    public static String close() { // "exit"
+        String args[] = new String[2];
+        args[0] = "\"requete\" : \"exit\"";
+        args[1] = "\"id\" : " + ++id + "";
+        String query = createQuery(args);
+        Connexion.getInstance().sendString(query);
+        return Connexion.getInstance().receiveString();
+    }
 }
